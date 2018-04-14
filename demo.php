e55a74d996f600b3a9768af5f44c505da42b6859
Austin Hull <AustinHull@users.noreply.github.com>
2018-04-14 18:35:35 -0400

    Format update

--- demo.php
+++ demo.php
@@ -1,4 +1,15 @@
 <html>
+    <?php
+        require 'db.php';
+        $res = mysqli_query($con, 'select * from plans;');
+        $row = mysqli_fetch_array($res);
+
+        $provider = $row['provider'].'</td>';
+        $tier = $row['tier'].'</td>';
+        $slow_insulin = $row['slow_act_insulin'].'</td>';
+        $fast_insulin = $row['fast_act_insulin'].'</td>';
+        $insulin_type = $row['insulin_type'].'</td>';
+    ?>
     <select>
         <option>Insurer 1</option>
         <option>Insurer 2</option>
@@ -7,17 +18,6 @@
     <button type='button'>Retrieve Provider Information</button>
     <table>
         <tr>
-            <?php
-            require 'db.php';
-            $res = mysqli_query($con, 'select * from plans;');
-            $row = mysqli_fetch_array($res);
-
-            echo '<td>'.$row['provider'].'</td>';
-            echo '<td>'.$row['tier'].'</td>';
-            echo '<td>'.$row['slow_act_insulin'].'</td>';
-            echo '<td>'.$row['fast_act_insulin'].'</td>';
-            echo '<td>'.$row['insulin_type'].'</td>';
-            ?>
         </tr>
     </table>
 </html>
