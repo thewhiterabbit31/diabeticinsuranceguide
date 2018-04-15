cfa8aad3eed2ff89ba40526bf82f3bac43288ebc
Al Masgai <alex.masgai@gmail.com>
2018-04-14 20:54:03 -0400

    Temp changes made

---
+++ mailer.php
@@ -0,0 +1,46 @@
+<?php
+
+require 'db.php';
+
+// Import PHPMailer classes into the global namespace
+// These must be at the top of your script, not inside a function
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+//Load Composer's autoloader
+require 'vendor/autoload.php';
+
+$mail = new PHPMailer(true);                              // Passing `true` enables exceptions
+try {
+    //Server settings
+    $mail->SMTPDebug = 2;                                 // Enable verbose debug output
+    $mail->isSMTP();                                      // Set mailer to use SMTP
+    $mail->Host = 'smtp.sendgrid.net';                    // Specify main and backup SMTP servers
+    $mail->SMTPAuth = true;                               // Enable SMTP authentication
+    $mail->Username = 'Philly-Codefest-Test';             // SMTP username
+    $mail->Password = '';                 // SMTP password
+    $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
+    $mail->Port = 587;                                    // TCP port to connect to
+
+    //Recipients
+    $mail->setFrom('diabeticInsurance@email.com', 'DI Email');
+    $mail->addAddress($_SESSION['email'], $_SESSION['name']);     // Add a recipient
+
+
+
+    //Content
+    $mail->isHTML(true);                                  // Set email format to HTML
+    $mail->Subject = 'Here is the subject';
+    $mail->Body    = 'Hello, we are reaching out to you
+    today inform you of the following changes listed below:' .
+      // Insert changes here via loop
+
+    // $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+    $mail->send();
+    echo 'Message has been sent';
+} catch (Exception $e) {
+    echo 'Message could not be sent. Mailer Error: ', $mail->ErrorInfo;
+}
+
+mysql_close($conn);
---
+++ texter.php
@@ -0,0 +1,29 @@
+<?php
+// Required if your environment does not handle autoloading
+require __DIR__ . '/vendor/autoload.php';
+
+// Use the REST API Client to make requests to the Twilio REST API
+use Twilio\Rest\Client;
+
+// Your Account SID and Auth Token from twilio.com/console
+$sid = 'AC7fde773f5362bf4d4f4a61314f93dd02';
+$token = 'YOUR API KEY HERE';
+$client = new Client($sid, $token);
+
+// Creating a list of updates found from database
+
+// Use the client to do fun stuff like send text messages!
+$client->messages->create(
+    // the number you'd like to send the message to
+    '+15558675309',
+    array(
+        // A Twilio phone number you purchased at twilio.com/console
+        'from' => '+15017250604',
+        // the body of the text message you'd like to send
+        // 'body' => 'Hey Jenny! Good luck on the bar exam!'
+
+        // Loop through changes and print them out. Then say whether or not the
+        // want to log in by providing a link to official website.
+        'body' => 'Hi, the following information was updated' .
+    )
+);
