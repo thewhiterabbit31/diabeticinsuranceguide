42a37ee7b28dee2610ad231f14497212b12f1def
Siling <testmiog@gmail.com>
2018-04-14 23:53:35 -0400

    update sql

--- submit.php
+++ submit.php
@@ -19,16 +19,94 @@ $pw = isset($_POST['pw']) ? $_POST['pw'] : null ;
 
 //search the plan that match
 if($diabetesType == 2){
-    $sql = "SELECT plan_id FROM type2_plans WHERE pill = 'Biguanides' ";
+    $sql1 = "SELECT plan_id, provider, tier FROM type2_plans WHERE pill = '".$pills ."'";
+    $res = mysqli_query($con, $sql1);
+    while ($row = mysqli_fetch_array($res)){
+        mysqli_query($con, 'INSERT INTO user_plans VALUES(' . $email. ',' . $row['plan_id'] .')' );
+
+        echo 'provider = '. $row['provider'] . '<br>';
+        echo 'tier = '. $row['tier'] . '<br>';
+    }
+    mailer($res);
 }
 
-if(!is_null ($pw)){
-    //user choose to create an account
+if($diabetesType == 1){
+    $sql1 = "SSELECT plan_id, provider, tier FROM 'type1_plans' 
+        WHERE (slow_act_insulin = '" .$slowInsulin. "' and fast_act_insulin = '" .$fastInsulin. "' and insulin_type = '" . $injectType . "');";
+    $res = mysqli_query($con, $sql1);
+    while ($row = mysqli_fetch_array($res)){
+        mysqli_query($con, 'INSERT INTO user_plans VALUES(' . $email. ',' . $row['plan_id'] .')' );
 
-    $sql = "INSERT INTO 'users' (email, diabetesType, )' ";
+        echo 'provider = '. $row['provider'] . '<br>';
+        echo 'tier = '. $row['tier'] . '<br>';
+    }
+    mailer($res);
+
 }
 
-$res = mysqli_query($con, $sql);
+// Import PHPMailer classes into the global namespace
+// These must be at the top of your script, not inside a function
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+function mailer($res){
+
+    //Load Composer's autoloader
+        require 'vendor/autoload.php';
+
+        $mail = new PHPMailer(true);                              // Passing `true` enables exceptions
+        try {
+            //Server settings
+            $mail->SMTPDebug = 2;                                 // Enable verbose debug output
+            $mail->isSMTP();                                      // Set mailer to use SMTP
+            $mail->Host = 'smtp.sendgrid.net';                    // Specify main and backup SMTP servers
+            $mail->SMTPAuth = true;                               // Enable SMTP authentication
+            $mail->Username = 'Philly-Codefest-Test';             // SMTP username
+            $mail->Password = '';                 // SMTP password
+            $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
+            $mail->Port = 587;                                    // TCP port to connect to
+
+            //Recipients
+            $mail->setFrom('diabeticInsurance@email.com', 'DI Email');
+            $mail->addAddress($_SESSION['email'], $_SESSION['name']);     // Add a recipient
+
+            $emailBody = "Hello, we are reaching out to you
+                today inform you of the following changes listed below:<br>
+                <table>
+                    <thead>
+                        <th>provider</th>
+                        <th>tier</th>
+                    </thead>
+                
+                ";
+            while ($row = mysqli_fetch_array($res)){
+                $emailBody = $emailBody."
+                <tr>
+                    <td>". $row['provider'] ."</td>
+                    <td>". $row['tier'] ."</td>
+                </tr>
+                ";
+            }
+
+            $emailBody = $emailBody."</table>";
+
+            //Content
+            $mail->isHTML(true);                                  // Set email format to HTML
+            $mail->Subject = 'Here is the subject';
+            $mail->Body    = $emailBody;
+
+                // Insert changes here via loo
+                // $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+            $mail->send();
+            echo 'Message has been sent';
+    }
+    catch (Exception $e) {
+        echo 'Message could not be sent. Mailer Error: ', $mail->ErrorInfo;
+    }
+}
+
+
 
 /*
 echo $type;
@@ -43,4 +121,6 @@ echo $pumpProvider;
 echo $pills;
 echo $email;
 */
+
+mysqli_close($con);
 ?>
