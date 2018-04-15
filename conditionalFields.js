0f6c81ba2d506c19de350fefe092977a092c2f5e
chrisyoungr <35730876+chrisyoungr@users.noreply.github.com>
2018-04-15 00:01:14 -0400

    Add files via upload

--- conditionalFields.js
+++ conditionalFields.js
@@ -1,79 +1,117 @@
 $( document ).ready(function() { //wait until body loads
 
 		//Inputs that determine what fields to show
-		var inject_type = $('#intake_form #inject_type').parent();
+		var inject_type = $('#intake_form #inject_type');
 		var diabetes_type = $('#intake_form input:radio[name=diabetesType]');
-		var optionsRadios = $('#intake_form input:radio[name=optionsRadios]');
+		var optionsUseInsulinRadios = $('#intake_form input:radio[name=optionsUseInsulinRadios]');
 		var optionsNotificationsRadios = $('#intake_form input:radio[name=optionsNotificationsRadios]');
 		var optionsPasswordRadios = $('#intake_form input:radio[name=optionsPasswordRadios]');
 		
 		//Wrappers for all fields
 		var pills = $('#intake_form #pills').parent();
 		var use_insulin = $('#intake_form #use_insulin');
-		var which_fast_insulin = $('#intake_form #which_fast_insulin').parent();
-		var which_slow_insulin = $('#intake_form #which_slow_insulin').parent();
+		var which_fast_insulin = $('#intake_form #which_fast_insulin');
+		var which_slow_insulin = $('#intake_form #which_slow_insulin');
 		var pump_provider = $('#intake_form #pump_provider').parent();
 		var size_gauge = $('#intake_form #size_gauge').parent();
 		var size_length  = $('#intake_form #size_length').parent();
 		var size_volume  = $('#intake_form #size_volume').parent();
 		var passwordField = $('#intake_form #passwordField').parent();
 		var phoneNumber = $('#intake_form #phoneNumber').parent();
-		
-		optionsRadios.change(function(){
-			var value=this.value;
-			if(value =='yes') {
-				which_fast_insulin.removeClass('hidden');
-				which_slow_insulin.removeClass('hidden');
-				inject_type.removeClass('hidden');
-			} 
-			if (value =='no') {
-				which_fast_insulin.addClass('hidden');
-				which_slow_insulin.addClass('hidden');
-				inject_type.addClass('hidden');
-				pills.removeClass('hidden');
-			}
-		});
 
 		diabetes_type.change(function(){
 			var value=this.value;
 			if(value =='2') {
 				pills.removeClass('hidden');
-				
+				use_insulin.removeClass('hidden');
+				use_insulin.find('input').prop('required',true);
 			} 
+			
 			if (value =='1') {
 				pills.addClass('hidden');
-				
+				use_insulin.addClass('hidden');
+			}
+		});
+		
+		optionsUseInsulinRadios.change(function(){
+			var value=this.value;
+			if(value =='yes') {
+				which_fast_insulin.parent().removeClass('hidden');
+				which_slow_insulin.parent().removeClass('hidden');
+				which_fast_insulin.prop('required',true);
+				which_slow_insulin.prop('required',true);
+				inject_type.parent().removeClass('hidden');
+			} 
+			if (value =='no') {
+				which_fast_insulin.parent().addClass('hidden');
+				which_slow_insulin.parent().addClass('hidden');
+				which_fast_insulin.prop('required',false);
+				which_slow_insulin.prop('required',false);
+				inject_type.parent().addClass('hidden');
+				pills.removeClass('hidden');
 			}
 		});
 
 		inject_type.change(function(){ //when the inject_type changes
 			var value=[];
 			$('#intake_form #inject_type option:selected').each(function(i, selected){ 
-              value[i] = $(selected).text(); 
-              alert(value[i]);
-              
-              if (value[i] == 'pen' || value[i] == 'vial'){
-              	alert("pen");
-              	size_length.removeClass('hidden');
-              	size_gauge.removeClass('hidden');
-              } else {
-              	size_length.removeClass('hidden');
-              	size_gauge.removeClass('hidden');
-              }
+				value[i] = $(selected).text(); 
+				console.log("value: " + value.length);
 
-              if(value[i] == 'pump') {
-              	alert("pump");
-              	pump_provider.removeClass('hidden');
-              } else {
-              	pump_provider.addClass('hidden')
-              }
-          });
+				if(value.length == 3) {
+					console.log("all");
+					size_length.removeClass('hidden');
+              		size_gauge.removeClass('hidden');
+              		pump_provider.removeClass('hidden');
+				} else if(value[i].length ==2) {
+					console.log("Length2");
+				} else {
+					if(value[i] == "Pen") {
+						console.log("Pen");
+						size_length.removeClass('hidden');
+						size_gauge.removeClass('hidden');
+					} else if(value[i] == "Vial") {
+						console.log("vial part of pen");
+						size_length.removeClass('hidden');
+						size_gauge.removeClass('hidden');
+					} else {
+						console.log("Hide Pen and Vial");
+						size_length.addClass('hidden');
+						size_gauge.addClass('hidden');
+					}
+
+					if(value[i] == "Vial") {
+						console.log("Vial");
+						size_length.removeClass('hidden');
+						size_gauge.removeClass('hidden');
+					} else if(value[i] == "Pen") {
+						console.log("pen part of vial");
+						size_length.removeClass('hidden');
+						size_gauge.removeClass('hidden');
+					} else {
+						console.log("Hide Pen and Vial");
+						size_length.addClass('hidden');
+						size_gauge.addClass('hidden');
+					}
+
+					if(value[i] == 'Pump') {
+						console.log("Pump");
+						pump_provider.removeClass('hidden');
+					} else {
+						pump_provider.addClass('hidden');
+					}
+				}
+
+			});
+			
+			
 		});	
 
 		optionsPasswordRadios.change(function(){
 			var value=this.value;
 			if(value =='yes') {
 				passwordField.removeClass('hidden');
+				passwordField.prop('required',true);
 				phoneNumber.removeClass('hidden');
 			} 
 			if (value =='no') {
